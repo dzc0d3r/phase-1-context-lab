56a9984b3b9d9fdb83eb00e25019b0a99daa45d3
dzc0d3r <m4dcod3r@protonmail.com>
2023-09-03 21:02:37 +0100

    Finished assignemet :tada:

--- index.js
+++ index.js
@@ -1,5 +1,58 @@
 /* Your Code Here */
 
+function createEmployeeRecord(array) {
+    return {
+        firstName: array[0],
+        familyName: array[1],
+        title: array[2],
+        payPerHour: array[3],
+        timeInEvents: [],
+        timeOutEvents: [],
+    };
+}
+
+function createEmployeeRecords(employeeData) {
+    return employeeData.map(createEmployeeRecord);
+}
+
+function createTimeInEvent(dateTime) {
+    const [date, hour] = dateTime.split(" ");
+    this.timeInEvents.push({ type: "TimeIn", date, hour: parseInt(hour, 10) });
+    return this;
+}
+
+function createTimeOutEvent(dateTime) {
+    const [date, hour] = dateTime.split(" ");
+    this.timeOutEvents.push({ type: "TimeOut", date, hour: parseInt(hour, 10) });
+    return this;
+}
+
+function hoursWorkedOnDate(date) {
+    const timeIn = this.timeInEvents.find((event) => event.date === date);
+    const timeOut = this.timeOutEvents.find((event) => event.date === date);
+    return (timeOut.hour - timeIn.hour) / 100;
+}
+
+function wagesEarnedOnDate(date) {
+    const hoursWorked = hoursWorkedOnDate.call(this, date);
+    return hoursWorked * this.payPerHour;
+}
+
+function allWagesFor() {
+    const datesWorked = this.timeInEvents.map((event) => event.date);
+    return datesWorked.reduce((totalWages, date) => totalWages + wagesEarnedOnDate.call(this, date), 0);
+}
+
+function findEmployeeByFirstName(employees, firstName) {
+    return employees.find((employee) => employee.firstName === firstName);
+}
+
+function calculatePayroll(employees) {
+    return employees.reduce((totalPayroll, employee) => totalPayroll + allWagesFor.call(employee), 0);
+}
+
+
+
 /*
  We're giving you this function. Take a look at it, you might see some usage
  that's new and different. That's because we're avoiding a well-known, but
@@ -9,7 +62,7 @@
  for you to use if you need it!
  */
 
-const allWagesFor = function () {
+function allWagesFor() {
     const eligibleDates = this.timeInEvents.map(function (e) {
         return e.date
     })
@@ -20,4 +73,18 @@ const allWagesFor = function () {
 
     return payable
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
